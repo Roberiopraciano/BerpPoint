52564d55a6a2959d1edb1fd4f9bd8b262147327f
HermesonxBerp <hermesonsantos@berp.com.br>
2025-06-09 19:03:38 -0300

    feat: implementing main.py

---
+++ app/main.py
@@ -0,0 +1,99 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.screen import MDScreen
+from kivy.uix.screenmanager import ScreenManager
+from kivy.core.window import Window
+from datetime import datetime
+
+Window.size = (360, 600)
+
+class MainScreen(MDScreen):
+    def show_recognized_user(self):
+
+        self.manager.current = 'user'
+        
+        employee = {
+            "photo": "./assets/test.jpg",
+            "name": "Francisco Hermeson",
+            "cpf": "123.456.789-10",
+        }
+
+        user_screen = self.manager.get_screen('user')
+        user_screen.ids.photo.source = employee["photo"]
+        user_screen.ids.name.text = f"Nome: {employee['name']}"
+        user_screen.ids.cpf.text = f"CPF: {employee['cpf']}"
+        user_screen.ids.datetime.text = f"Data e Hora: {datetime.now().strftime('%d/%m/%Y às %H:%M')}"
+
+        user_screen.ids.card.opacity = 1
+
+
+class UserScreen(MDScreen):
+    pass
+
+
+class ReceiptScreen(MDScreen):
+    pass
+
+
+class ScreenManagerApp(ScreenManager):
+    def show_recognized_user(self):
+        self.get_screen('main').show_recognized_user()
+
+
+class MainApp(MDApp):
+    def build(self):
+        return Builder.load_string("""
+ScreenManagerApp:
+    MainScreen:
+    UserScreen:
+    ReceiptScreen:
+                                                        
+<MainScreen>:
+    name: "main"
+                                   
+    MDScreen:
+        md_bg_color: 0.941, 0.957, 0.973, 1
+
+        MDTopAppBar:
+            title: "Reconhecimento"
+            pecific_text_color: 1, 1, 1, 1
+            anchor_title: "center"
+            md_bg_color: 0.173, 0.243, 0.314, 1
+            elevation: 0.5
+            pos_hint: {"top": 1}
+                                   
+        MDBoxLayout:
+            orientation: "vertical"
+            adaptive_size: True
+            spacing: "20dp"
+            pos_hint: {"center_x": 0.5, "center_y": 0.6}
+
+            MDCard:
+                id: headimage
+                size_hint: None, None
+                size: "300dp", "300dp"
+                pos_hint: {"center_x": 0.5}
+
+                AsyncImage:
+                    size_hint: (1, 1)
+                    pos_hint: {'center_x': 0.5}
+                    source: './assets/teste.jpg'
+                                   
+        MDRaisedButton:
+            text: 'Registrar'
+            font_size: '20sp'
+            pos_hint: {'center_x': 0.5, 'center_y': 0.25}
+            md_bg_color: 1, 0.388, 0.278, 1
+            size_hint: (0.7, 0.1)
+            elevation: 0.5
+            on_press: root.show_recognized_user()
+                                   
+<UserScreen>:
+    name: user
+                                   
+<ReceiptScreen>:
+    name: receipt
+""")
+
+if __name__ == '__main__':
+    MainApp().run()
